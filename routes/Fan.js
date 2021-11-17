bea53ac743387f8714e2ded37738c3bc8127f912
Abhishikth Dweeti <85538905+Abhishikthdweeti@users.noreply.github.com>
2021-11-17 17:36:25 -0600

    creating a new instance of your collection

--- routes/Fan.js
+++ routes/Fan.js
@@ -6,4 +6,7 @@ router.get('/', fan_controlers.fan_view_all_Page);
 
 /* GET detail fan page */
 router.get('/detail', fan_controlers.fan_view_one_Page);
+
+/* GET create fan page */
+router.get('/create', fan_controlers.fan_create_Page);
 module.exports = router;
