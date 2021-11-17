7950384c3330e3f073088d1b9ca0bb7461780b94
Abhishikth Dweeti <85538905+Abhishikthdweeti@users.noreply.github.com>
2021-11-17 17:52:04 -0600

    deleting an instance of your collection

--- controllers/Fan.js
+++ controllers/Fan.js
@@ -135,3 +135,16 @@ exports.fan_update_Page =  async function(req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle a delete one view with id from query
+exports.fan_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await fan.findById(req.query.id)
+        res.render('fandelete', { title: 'fan Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
